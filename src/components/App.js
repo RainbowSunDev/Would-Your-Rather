2fe60007f1839d758df052221674e945f3efdd84
thsblue <thsbluehsg@gmail.com>
2020-07-10 16:55:34 +0200

    Added Homepage Layout

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,29 @@ import { handleInitialData } from "../actions/shared";
 import { connect } from "react-redux";
 
 import NavBar from "./Nav";
-
+import HomePage from "./HomePage";
 class App extends Component {
 	componentDidMount() {
 		console.log(this.props);
 		this.props.dispatch(handleInitialData());
 	}
 	render() {
+		const { authUser } = this.props;
 		return (
 			<div className="app">
 				<Router>
 					<NavBar />
+					{
+						//authUser == null && <HomePage />}
+						//TODO When Completing the sign in methods this will be the code
+					}
+					<HomePage />
 				</Router>
 			</div>
 		);
 	}
 }
-
-export default connect()(App);
+function mapStateToProps({ authUser }) {
+	return { authUser };
+}
+export default connect(mapStateToProps)(App);
