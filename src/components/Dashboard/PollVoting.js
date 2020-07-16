faf202ccee5d88ee4a2c11092db92a6a6e54ff9c
thsblue <thsbluehsg@gmail.com>
2020-07-16 15:53:24 +0200

    Added Pie Charts

--- src/components/Dashboard/PollVoting.js
+++ src/components/Dashboard/PollVoting.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import { vote } from "../../actions/questions";
+import { firestoreConnect } from "react-redux-firebase";
+import { Pie, Doughnut } from "react-chartjs-2";
 
 import "./poll_vote.scss";
 import "./poll_vote.css";
 import { connect } from "react-redux";
+import { compose } from "redux";
+
 class PollVoting extends Component {
 	constructor(props) {
 		super(props);
@@ -35,52 +39,108 @@ class PollVoting extends Component {
 
 		this.props.vote(id, option, loggedInUser);
 	};
+
+	unansweredPolls(
+		optionTwoChecked,
+		optionOneText,
+		optionOneChecked,
+		optionTwoText,
+		answered
+	) {
+		console.log(answered);
+		return (
+			<form className="form">
+				<div className="inputGroup">
+					<input
+						id="option1"
+						name="option1"
+						type="checkbox"
+						disabled={optionTwoChecked === true || answered}
+						onChange={this.toggleOption1Checked}
+						checked={answered === "optionOne"}
+					/>
+					<label htmlFor="option1">{optionOneText}</label>
+				</div>
+
+				<div className="inputGroup">
+					<input
+						id="option2"
+						name="option2"
+						type="checkbox"
+						onChange={this.toggleOption2Checked}
+						disabled={optionOneChecked === true || answered}
+						checked={answered === "optionTwo"}
+					/>
+					<label htmlFor="option2">{optionTwoText}</label>
+				</div>
+				<button
+					type="submit"
+					className="add-new-poll vote-btn"
+					disabled={optionOneChecked === optionTwoChecked || answered}
+					onClick={this.handleVoting}
+				>
+					VOTE
+				</button>
+			</form>
+		);
+	}
+
 	render() {
 		const {
 			author,
-			uid,
 			id,
 			optionOneText,
 			optionTwoText,
-			timestamp,
+			optionOneVotes,
+			optionTwoVotes,
 		} = this.props.selectedQuestion;
+		const { authedUid, userAnswers } = this.props;
 		const { optionOneChecked, optionTwoChecked } = this.state;
-		console.log(this.state);
+
+		console.log(this.props);
+		const answered = Object.keys(userAnswers).includes(id)
+			? userAnswers[id]
+			: false;
+		const state = {
+			labels: ["Option 1", "Option 2"],
+			datasets: [
+				{
+					backgroundColor: ["#FE2F57", "#97DAC7"],
+
+					data: [optionOneVotes.length, optionTwoVotes.length],
+				},
+			],
+		};
 		return (
 			<div className="poll-voting-container">
-				<h2>{author} Asks</h2>
-				<span>Would You Rather.. </span>
-				<form className="form">
-					<div className="inputGroup">
-						<input
-							id="option1"
-							name="option1"
-							type="checkbox"
-							disabled={optionTwoChecked === true}
-							onClick={this.toggleOption1Checked}
-						/>
-						<label htmlFor="option1">{optionOneText}</label>
-					</div>
+				<div>
+					<h2>{author} Asks</h2>
+					<span>Would You Rather.. </span>
 
-					<div className="inputGroup">
-						<input
-							id="option2"
-							name="option2"
-							type="checkbox"
-							onClick={this.toggleOption2Checked}
-							disabled={optionOneChecked === true}
-						/>
-						<label htmlFor="option2">{optionTwoText}</label>
-					</div>
-					<button
-						type="submit"
-						className="add-new-poll vote-btn"
-						disabled={optionOneChecked === optionTwoChecked}
-						onClick={this.handleVoting}
-					>
-						VOTE
-					</button>
-				</form>
+					{this.unansweredPolls(
+						optionTwoChecked,
+						optionOneText,
+						optionOneChecked,
+						optionTwoText,
+						answered
+					)}
+				</div>
+
+				<Pie
+					className={"pie-chart"}
+					width={"400px"}
+					height={"220px"}
+					data={state}
+					options={{
+						pieStartAngle: 100,
+						pieSliceText: "label",
+						responsive: true,
+						legend: {
+							position: "right",
+							color: "#FFF",
+						},
+					}}
+				/>
 			</div>
 		);
 	}
@@ -89,7 +149,8 @@ const mapStateToProps = (state) => {
 	console.log("__THE STATE OF THE VOTING CMP", state);
 	const uid = state.firebase.auth.uid;
 	return {
-		loggedInUser: uid,
+		authedUid: uid,
+		userAnswers: state.firestore.data.users[uid].answers,
 	};
 };
 const mapDispatchToProps = (dispatch) => {
@@ -99,4 +160,7 @@ const mapDispatchToProps = (dispatch) => {
 	};
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(PollVoting);
+export default compose(
+	connect(mapStateToProps, mapDispatchToProps),
+	firestoreConnect([{ collection: "users" }])
+)(PollVoting);
