e8643a289ba64672d3ad3be25220d77c74d42034
thsblue <thsbluehsg@gmail.com>
2020-07-17 03:05:24 +0200

    Added a collapsible component to the voting component

--- src/components/Dashboard/PollVoting.js
+++ src/components/Dashboard/PollVoting.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { vote } from "../../actions/questions";
 import { firestoreConnect } from "react-redux-firebase";
-import { Pie, Doughnut } from "react-chartjs-2";
+import { Pie } from "react-chartjs-2";
+import Collapsible from "react-collapsible";
 
 import "./poll_vote.scss";
 import "./poll_vote.css";
@@ -11,7 +12,11 @@ import { compose } from "redux";
 class PollVoting extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { optionOneChecked: false, optionTwoChecked: false };
+		this.state = {
+			optionOneChecked: false,
+			optionTwoChecked: false,
+			showResult: false,
+		};
 	}
 	toggleOption1Checked = () => {
 		this.setState((prevState) => {
@@ -39,6 +44,12 @@ class PollVoting extends Component {
 
 		this.props.vote(id, option, loggedInUser);
 	};
+	handleShowResult = (e) => {
+		e.preventDefault();
+		this.setState((prevState) => {
+			return { showResult: !prevState.showResult };
+		});
+	};
 
 	unansweredPolls(
 		optionTwoChecked,
@@ -48,16 +59,16 @@ class PollVoting extends Component {
 		answered
 	) {
 		console.log(answered);
-		return (
+
+		return answered === false ? (
 			<form className="form">
 				<div className="inputGroup">
 					<input
 						id="option1"
 						name="option1"
 						type="checkbox"
-						disabled={optionTwoChecked === true || answered}
 						onChange={this.toggleOption1Checked}
-						checked={answered === "optionOne"}
+						disabled={optionTwoChecked === true}
 					/>
 					<label htmlFor="option1">{optionOneText}</label>
 				</div>
@@ -68,19 +79,49 @@ class PollVoting extends Component {
 						name="option2"
 						type="checkbox"
 						onChange={this.toggleOption2Checked}
-						disabled={optionOneChecked === true || answered}
-						checked={answered === "optionTwo"}
+						disabled={optionOneChecked === true}
 					/>
 					<label htmlFor="option2">{optionTwoText}</label>
 				</div>
 				<button
 					type="submit"
 					className="add-new-poll vote-btn"
-					disabled={optionOneChecked === optionTwoChecked || answered}
+					disabled={optionOneChecked === optionTwoChecked}
 					onClick={this.handleVoting}
 				>
 					VOTE
 				</button>
+			</form>
+		) : (
+			<form className="form">
+				<div className="inputGroup">
+					<input
+						id="option1"
+						name="option1"
+						type="checkbox"
+						onChange={this.toggleOption1Checked}
+						checked={answered === "optionOne"}
+						disabled={true}
+					/>
+					<label htmlFor="option1">{optionOneText}</label>
+				</div>
+
+				<div className="inputGroup">
+					<input
+						id="option2"
+						name="option2"
+						type="checkbox"
+						onChange={this.toggleOption2Checked}
+						checked={answered === "optionTwo"}
+						disabled={true}
+					/>
+					<label htmlFor="option2">{optionTwoText}</label>
+				</div>
+				<div
+					style={{ "font-size": "14px", color: "rgba(255, 255, 255, 0.734)" }}
+				>
+					you have already answered this question
+				</div>
 			</form>
 		);
 	}
@@ -95,7 +136,7 @@ class PollVoting extends Component {
 			optionTwoVotes,
 		} = this.props.selectedQuestion;
 		const { authedUid, userAnswers } = this.props;
-		const { optionOneChecked, optionTwoChecked } = this.state;
+		const { optionOneChecked, optionTwoChecked, showResult } = this.state;
 
 		console.log(this.props);
 		const answered = Object.keys(userAnswers).includes(id)
@@ -106,7 +147,6 @@ class PollVoting extends Component {
 			datasets: [
 				{
 					backgroundColor: ["#FE2F57", "#97DAC7"],
-
 					data: [optionOneVotes.length, optionTwoVotes.length],
 				},
 			],
@@ -125,22 +165,32 @@ class PollVoting extends Component {
 						answered
 					)}
 				</div>
-
-				<Pie
-					className={"pie-chart"}
-					width={"400px"}
-					height={"220px"}
-					data={state}
-					options={{
-						pieStartAngle: 100,
-						pieSliceText: "label",
-						responsive: true,
-						legend: {
-							position: "right",
-							color: "#FFF",
-						},
-					}}
-				/>
+				{answered && (
+					<Collapsible
+						className="collapse-result"
+						openedClassName="collapse-result"
+						trigger="Show Results"
+						triggerWhenOpen="Hide Results"
+						triggerTagName="button"
+						triggerDisabled={answered === false}
+					>
+						<Pie
+							width={"400px"}
+							height={"220px"}
+							data={state}
+							title="Poll Result"
+							options={{
+								pieStartAngle: 100,
+								pieSliceText: "label",
+								responsive: true,
+								legend: {
+									position: "left",
+									color: "#FFF",
+								},
+							}}
+						/>
+					</Collapsible>
+				)}
 			</div>
 		);
 	}
